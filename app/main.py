b45e16234f77775113e7c5f806f0359e0d3505e5
Ayomide <ay0m1ealonge@gmail.com>
2025-11-11 12:29:07 +0000

    linking frontend to cloud hosted backend

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ app = FastAPI(title="Mental Health API")
 
 origins = [
     # for Live Server in VS Code
+    "https://ay0m1e.github.io/mental_healthproj/",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://127.0.0.1:5501",
--- script.js
+++ script.js
@@ -20,7 +20,7 @@ async function fetchAffirmation(mood = "neutral") {
   try {
     console.log("Sending mood:", mood);
 
-    const res = await fetch("http://127.0.0.1:8000/api/mood", {
+    const res = await fetch("https://mental-healthproj.onrender.com/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ mood }),
